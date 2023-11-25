6fc6c1ee937a93dbb29685ee7549499e29f6ed30
Diego Toro <diegotoro6789@gmail.com>
2023-11-24 19:43:37 -0500

    v2

--- AppFront/src/app/service/beaches.service.ts
+++ AppFront/src/app/service/beaches.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class BeachesService {
   constructor(private httpClient: HttpClient) { }
 
   getAll(): Observable<any[]> {
-    return this.httpClient.get<any[]>('3.140.251.106:8080/api/beaches');
+    return this.httpClient.get<any[]>(environment.ipPublic + '/api/beaches');
   }
 }
---
+++ AppFront/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+
+   ipPublic: 'http://18.118.13.206:8080'
+};
